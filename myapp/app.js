83c2566ca0891cdfd254bf42730a930ea9362db9
book <bookgvi@gmail.com>
2020-01-07 14:20:50 +0300

    simple middleWare

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+const simpleMiddlware = (req, res, next) => {
+  req.currTime = new Date()
+  next();
+}
+
+app.use(simpleMiddlware)
+
 app.get('/', (req, resp) => {
-  resp.send('Hello, motherfucker')
+  resp.send(`Hello, motherfucker, ${req.currTime}`)
 })
 
 app.listen(3000, () => {
