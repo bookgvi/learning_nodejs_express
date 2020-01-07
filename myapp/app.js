db932909a71d00e4722d60ed5ee53adc9e7869c9
book <bookgvi@gmail.com>
2020-01-07 15:59:35 +0300

    изучение ответов сервера

--- myapp/app.js
+++ myapp/app.js
@@ -1,15 +1,19 @@
 const express = require('express')
-const app = express()
+const http = require('http')
 
 const simpleMiddlware = (req, res, next) => {
   req.currTime = new Date()
   next();
 }
 
-app.use(simpleMiddlware)
-
-app.get('/', (req, resp) => {
-  resp.send(`Hello, motherfucker, ${req.currTime}`)
+const app = http.createServer((req, res) => {
+  if (req.url === '/about') {
+    const response = JSON.stringify({ errors: [{ source: 'login' }]})
+    res.statusCode = 401
+    res.setHeader('Content-Type', 'application/json')
+    res.write(response)
+    res.end()
+  }
 })
 
 app.listen(3000, () => {
